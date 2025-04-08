4dac6bac839a2863bc41447329f3fa559a1b8bb4
Jason Lyu <xjasonlyu@gmail.com>
2025-04-07 22:30:41 -0400

    Refactor: language functions (#4)

---
+++ lang.go
@@ -0,0 +1,35 @@
+package openaitranslator
+
+import (
+	"sync"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/language/display"
+)
+
+var _languages = sync.Map{}
+
+func RegisterLanguage(code, lang string) {
+	_languages.Store(code, lang)
+}
+
+func LookupLanguage(code string) string {
+	if code == "" {
+		return ""
+	}
+	if lang, ok := _languages.Load(code); ok {
+		return lang.(string)
+	}
+	tag, err := language.Parse(code)
+	if err != nil {
+		return code // fallback to original language code.
+	}
+	eng := language.English
+	return display.Tags(eng).Name(tag)
+}
+
+func init() {
+	RegisterLanguage("wyw", "中文（古文-文言文）")
+	RegisterLanguage("chs", "Simplified Chinese")
+	RegisterLanguage("cht", "Traditional Chinese")
+}
---
+++ lang_test.go
@@ -0,0 +1,56 @@
+package openaitranslator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLookupLanguage(t *testing.T) {
+	tests := []struct {
+		code, lang string
+	}{
+		{"en", "English"},
+		{"eng", "English"},
+		{"fra", "French"},
+		{"en-US", "American English"},
+		{"en-CA", "Canadian English"},
+		{"en-GB", "British English"},
+		{"ja", "Japanese"},
+		{"zh", "Chinese"},
+		{"zh-Hans", "Simplified Chinese"},
+		{"zh-Hant", "Traditional Chinese"},
+		{"zh-CN", "Chinese (China)"},
+		{"zh_cn", "Chinese (China)"},
+		{"zh-TW", "Chinese (Taiwan)"},
+		{"yue", "Cantonese"},
+		// fallback languages
+		{"unknown", "unknown"},
+		// registered languages
+		{"wyw", "中文（古文-文言文）"},
+		{"chs", "Simplified Chinese"},
+		{"cht", "Traditional Chinese"},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, LookupLanguage(tt.code), tt.lang)
+	}
+}
+
+func TestRegisterLanguage(t *testing.T) {
+	tests := []struct {
+		code, lang string
+	}{
+		{"code1", "Code 1"},
+		{"code2", "Code 2"},
+		{"code3", "Code 3"},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, LookupLanguage(tt.code), tt.code)
+	}
+	for _, tt := range tests {
+		RegisterLanguage(tt.code, tt.lang)
+	}
+	for _, tt := range tests {
+		assert.Equal(t, LookupLanguage(tt.code), tt.lang)
+	}
+}
