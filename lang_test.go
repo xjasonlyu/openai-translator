41e51f6baf66b99de3c7fbe7564dea68063dd3c0
xjasonlyu <xjasonlyu@gmail.com>
2025-04-07 21:49:33 -0400

    Refactor: lookup languages

--- lang_test.go
+++ lang_test.go
@@ -1,9 +1,31 @@
 package openaitranslator
 
-import "testing"
+import (
+	"testing"
 
-func TestRegisterLanguage(t *testing.T) {
-	t.Log(GetLangMap())
-	RegisterLanguage("zh-CN", "简体中文")
-	t.Log(GetLangMap())
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLookupLanguage(t *testing.T) {
+	tests := []struct {
+		code, lang string
+	}{
+		{"en", "English"},
+		{"en-US", "American English"},
+		{"en-CA", "Canadian English"},
+		{"en-GB", "British English"},
+		{"zh", "Chinese"},
+		{"zh-Hans", "Simplified Chinese"},
+		{"zh-Hant", "Traditional Chinese"},
+		{"zh-CN", "Chinese (China)"},
+		{"zh-TW", "Chinese (Taiwan)"},
+		{"yue", "Cantonese"},
+		// fallback languages
+		{"unknown", "unknown"},
+		// registered languages
+		{"wyw", "中文（古文-文言文）"},
+	}
+	for _, tt := range tests {
+		assert.Equal(t, LookupLanguage(tt.code), tt.lang)
+	}
 }
